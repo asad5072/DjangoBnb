f91e2ee1d961f6f353c8e699b11539107e05b3cd
Enamulx <coderxdante@gmail.com>
2025-03-23 16:19:41 +0600

    asad

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import "./globals.css";
+
+export default function Home() {
+	return (
+		<div className="">
+			<h1>Hello Wrold!</h1>
+			<p className="text-airbnb">Text-color text</p>
+		</div>
+	);
+}
